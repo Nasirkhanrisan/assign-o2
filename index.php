45e998047a1f7d2af8dc09035ab5752d9a77f840
unknown <mdnasirkhan7151@gmail.com>
2022-03-25 18:00:00 +0600

    All File Done

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$hello = "Nasir khan"; // String
+$dist = 'Tangail'; //String
+
+echo $hello;
+echo "<br>";
+echo $dist;
+
+echo "<br>";
+
+$number = 5985; // Integer
+echo $number ;
+
+echo "<br>";
+$number_1 = 123456789; // Integer
+echo $number_1 ;
+
+echo "<br>";
+$flot = 10.365; //Float
+echo $flot;
+
+echo "<br>";
+$true = true; // Boolean
+$false = false;
+
+echo "<br>";
+
+$name =array("My", "Name", "Is", "Nasir" ,"Khan"); // Array
+echo $name[3];
+
+echo "<br>";
+
+$null = "Nasir khan"; //Null
+$null = null;
+var_dump($null);
+
+echo "<br>";
+echo strlen("Nasir khan"); //String Functions
+?>
+
